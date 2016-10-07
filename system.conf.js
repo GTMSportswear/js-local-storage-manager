203fb2e58712d37eb42f59685dfe781495f12dbe
Nick George <me@izzmo.com>
2016-10-07 12:20:19 -0500

    Added some libraries for testing harness.

---
+++ system.conf.js
@@ -0,0 +1,9 @@
+﻿System.config({
+  baseURL: '/',
+  paths: {
+    systemjs: 'lib/system.js/dist/system.js',
+    traceur: 'lib/traceur/traceur.min.js'
+  },
+  defaultJSExtensions: true,
+  transpiler: 'traceur'
+});
